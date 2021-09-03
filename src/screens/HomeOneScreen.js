5b0c60a07995db0478f805efd2b73c867f33010d
adeltahenti <adel.tahenti@citycable.ch>
2021-09-03 15:32:44 +0200

    Second Image

--- src/screens/HomeOneScreen.js
+++ src/screens/HomeOneScreen.js
@@ -2,14 +2,14 @@ const HomeOneScreen = () => {
   return (
     <div
       className='bg-cover bg-no-repeat bg-center min-h-screen relative'
-      style={{ backgroundImage: 'url(/images/img2.jpg)' }}
+      style={{ backgroundImage: 'url(/images/img3.jpg)' }}
     >
       {/* <div className='absolute bg-gradient-to-b from-gray-100 to-gray-200 opacity-25 inset-0 z-0'></div> */}
       <div className='flex flex-col items-center justify-center h-screen'>
-        <span className='text-white text-2xl md:text-3xl lg:text-5xl animate__animated animate__fadeInLeft animate__delay-0.5s'>
+        <span className='text-gray-200 font-bold text-2xl md:text-3xl lg:text-5xl animate__animated animate__fadeInLeft animate__delay-0.5s'>
           Ma page d'accueil
         </span>
-        <p className='text-white mt-3 animate__animated animate__fadeInRight md:px-3 text-center px-2'>
+        <p className='text-gray-200 font-boldmt-3 animate__animated animate__fadeInRight md:px-3 text-center px-2'>
           Avec une photo comme fond plus un filtre, en sachant que tu choisir ta
           photo ainsi que le filtre. Aussi la couleur du texte peut variée.
         </p>
